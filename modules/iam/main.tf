6e449555a0c1a38b8317572a7656dcc297f3fd66
mahamatmbodou <mahamatmbodou33@gmail.com>
2026-05-28 12:18:27 -0400

    iam module changed

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -20,14 +20,13 @@ resource "aws_iam_role" "github_actions_role" {
           }
 
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_owner}/${var.github_repo}:*"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_owner}/${var.github_repo}:ref:refs/heads/*"
           }
         }
       }
     ]
   })
 }
-
 resource "aws_iam_role" "ec2_app_role" {
   name = "${var.project_name}-${var.environment}-ec2-app-role"
 
