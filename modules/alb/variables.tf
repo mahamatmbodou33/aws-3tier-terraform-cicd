8ac90ee3094ffd1de72dd1259cd30ef46952634c
mahamatmbodou <mahamatmbodou33@gmail.com>
2026-08-30 00:40:44 -0400

    updated gitignore

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,6 +1,12 @@
-variable "vpc_id" {}
-variable "public_subnets" { type = list(string) }
-variable "alb_sg_id" {}
+variable "vpc_id" {
+  type = string
+}
+variable "public_subnets" {
+  type = list(string)
+}
+variable "alb_sg_id" {
+  type = string
+}
 variable "tags" {
   type = map(string)
 }
